7e6cb703f65c754c69308f09dc15934a18d75c70
Karsten Schmidt <k@postspectacular.com>
2016-06-07 16:31:34 +0100

    update ex03

--- day2/ex03/src/ex03/core.clj
+++ day2/ex03/src/ex03/core.clj
@@ -1,4 +1,5 @@
 (ns ex03.core
+  (:gen-class)
   (:require
    [org.httpkit.server :as http]
    [compojure.core :refer :all]
@@ -11,7 +12,11 @@
    [ring.middleware.stacktrace :refer [wrap-stacktrace]]
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [ring.util.response :as resp]
-   [ring.util.mime-type :refer [default-mime-types]]))
+   [ring.util.mime-type :refer [default-mime-types]]
+   [environ.core :refer [env]]
+   [thi.ng.strf.core :as f]
+   [thi.ng.validate.core :as v]
+   [clojure.data.json :as json]))
 
 (defonce state (atom {}))
 
@@ -27,13 +32,46 @@
     [:title "ws-ldn-9"]
     (include-css "//fonts.googleapis.com/css?family=Roboto" "/css/main.css")]))
 
+(defn page
+  [& body]
+  (html5
+   {:lang "en"}
+   html-head
+   [:body body]))
+
+(def coercions-types
+  {:int #(f/parse-int % 10 -1)
+   :float #(f/parse-float % -1)})
+
+;; (coerce-params (:params req) {:id :int :age :int})
+;; [[:id :int] [:age :int]]
+
+(defn coerce-params
+  [params coercions]
+  (reduce
+   (fn [acc [param type]]
+     (if (coercions-types type)
+       (update acc param (coercions-types type))
+       acc))
+   params
+   coercions))
+
 (defroutes app-routes
   (GET "/" [:as req]
-       (html5
-        {:lang "en"}
-        html-head
-        [:body
-         [:h1 "Hello world!!!"]]))
+       (page [:h1 "Hello world!!!"]))
+  (GET ["/people/:id" :id #"\d+"] [id]
+       (page [:h1 id]))
+  (GET "/users/:id" [:as request]
+       (let [[params err] (-> request
+                              :params
+                              (coerce-params {:id :int})
+                              (v/validate {:id [(v/number) (v/pos)]}))]
+         (if err
+           {:status 400
+            :body (first (:id err))}
+           (-> {:status 200
+                :body (json/write-str {:answer #{(:id params) "some other things"}})}
+               (resp/content-type (default-mime-types "json"))))))
   (route/not-found "Not Found"))
 
 (def app
@@ -44,7 +82,9 @@
 
 (defn start!
   []
-  (swap! state assoc :server (http/run-server #'app {:port 3000})))
+  (let [port (f/parse-int (env :port) 10 3000)]
+    (swap! state assoc :server (http/run-server #'app {:port port}))
+    (println "server running @" port)))
 
 (defn stop!
   []
