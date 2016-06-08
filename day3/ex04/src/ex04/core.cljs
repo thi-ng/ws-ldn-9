7095947e3a11d62a09c3f4feead4fd7968a40145
Karsten Schmidt <k@postspectacular.com>
2016-06-08 09:36:23 +0100

    add ex04

---
+++ day3/ex04/src/ex04/core.cljs
@@ -0,0 +1,58 @@
+(ns ex04.core
+  (:require-macros
+   [reagent.ratom :refer [reaction]]
+   [cljs-log.core :refer [debug info warn severe]])
+  (:require
+   [ex04.state :as state]
+   [ex04.router :as router]
+   [ex04.home :as home]
+   [reagent.core :as reagent]))
+
+(enable-console-print!)
+
+(def routes
+  "Basic SPA route configuration. See router ns for further options."
+  [{:id        :home
+    :match     ["home"]
+    :component #'home/home
+    :label     "Home"}])
+
+(defn view-wrapper
+  "Shared component wrapper for all routes, includes navbar."
+  [route]
+  (let [route @route]
+    [(:component route) route]))
+
+(defn app-component
+  "Application main component."
+  []
+  (let [route (reaction (:curr-route @state/app))]
+    (fn []
+      (if (and @route)
+        [view-wrapper route]
+        [:div "initializing..."]))))
+
+(defn start-router
+  "Starts SPA router, called from main fn."
+  []
+  (router/start!
+   routes
+   nil
+   (router/route-for-id routes :home)
+   state/nav-change
+   (constantly nil)))
+
+(defn main
+  "Application main entry point, initializes app state and kicks off
+  React component lifecycle."
+  []
+  (when-not (:inited @state/app)
+    (state/init-app))
+  (start-router)
+  (reagent/render-component [app-component] (.getElementById js/document "app")))
+
+(defn on-js-reload
+  "Called each time fighweel has reloaded code."
+  [] (debug :reloaded))
+
+(main)
---
+++ day3/ex04/src/ex04/state.cljs
@@ -0,0 +1,39 @@
+(ns ex04.state
+  (:require-macros
+   [reagent.ratom :refer [reaction]]
+   [cljs-log.core :refer [debug info warn]])
+  (:require
+   [reagent.core :as reagent]))
+
+(defonce app (reagent/atom {}))
+
+(defn set-state!
+  "Helper fn to reset app state for given key. Key can be single
+  keyword or path vector."
+  [key val]
+  (debug key val)
+  (swap! app (if (sequential? key) assoc-in assoc) key val))
+
+(defn update-state!
+  "Helper fn to update app state value for given key. Key can be single
+  keyword or path vector. Behaves like clojure.core/update or update-in"
+  [key f & args]
+  (debug key args)
+  (swap! app #(apply (if (sequential? key) update-in update) % key f args)))
+
+(defn subscribe
+  "Helper fn to create a reagent reaction for given app state key/path."
+  [key]
+  (if (sequential? key)
+    (reaction (get-in @app key))
+    (reaction (@app key))))
+
+(defn nav-change
+  [route]
+  (set-state! :curr-route route)
+  (set-state! :nav-open? false))
+
+(defn init-app
+  []
+  (swap! app merge
+         {:inited true}))
