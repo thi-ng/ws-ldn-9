9540dfc364c8d2a31b150dca7789b42234ba31bf
Karsten Schmidt <k@postspectacular.com>
2016-06-08 16:01:42 +0100

    add canvas events

--- day3/ex04/src/ex04/canvas.cljs
+++ day3/ex04/src/ex04/canvas.cljs
@@ -2,8 +2,10 @@
   (:require-macros
    [cljs-log.core :refer [debug info warn severe]])
   (:require
+   [ex04.state :as state]
    [reagent.core :as reagent]
    [thi.ng.geom.gl.webgl.animator :as anim]
+   [thi.ng.math.core :as m]
    [thi.ng.color.core :as col]))
 
 (defn canvas-component
@@ -26,19 +28,40 @@
          :height (.-innerHeight js/window)}
         props)])}))
 
+(defn draw-dot
+  [e]
+  (let [canvas (.-target e)
+        ctx    (.getContext canvas "2d")
+        offx   (.-offsetLeft canvas)
+        offy   (.-offsetTop canvas)
+        x      (- (.-clientX e) offx)
+        y      (- (.-clientY e) offy)]
+    (set! (.-fillStyle ctx) (-> (col/random-rgb) (col/as-css) deref))
+    (doto ctx
+      (.beginPath)
+      (.arc x y 20 0 m/TWO_PI false)
+      (.fill)
+      (.closePath))))
+
 (defn canvas
   [route]
   [canvas-component
-   {:width 640
-    :height 480
-    :init (fn [comp] (debug "init canvas"))
-    :loop (fn [comp]
-            (fn []
-              (let [canvas (reagent/dom-node comp)
-                    ctx (.getContext canvas "2d")]
-                (set! (.-fillStyle ctx) (-> (col/random-rgb) (col/as-css) deref))
-                (set! (.-font ctx) "32px Arial")
-                (.fillRect ctx 0 0 (.-width canvas) (.-height canvas))
-                (set! (.-fillStyle ctx) (-> (col/random-rgb) (col/as-css) deref))
-                (.fillText ctx "Epilepsy warning" 100 100)
-                (:active (reagent/state comp)))))}])
+   {:width         640
+    :height        480
+    :init          (fn [comp]
+                     (debug "init canvas"))
+    :loop          (fn [comp]
+                     (let [canvas (reagent/dom-node comp)
+                           ctx (.getContext canvas "2d")]
+                       (set! (.-fillStyle ctx) "blue"
+                             #_(-> (col/random-rgb) (col/as-css) deref))
+                       (.fillRect ctx 0 0 (.-width canvas) (.-height canvas))
+                       (fn [] (:active (reagent/state comp)))))
+    :on-mouse-down (fn [e]
+                     (state/set-state! :clicked true)
+                     (draw-dot e))
+    :on-mouse-up   (fn [e]
+                     (state/set-state! :clicked false))
+    :on-mouse-move (fn [e]
+                     (when (:clicked @state/app)
+                       (draw-dot e)))}])
